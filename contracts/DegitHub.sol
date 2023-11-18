e5183bad32c13312e781adcb6c76fe722e0d1241
Marios Isaakidis <prometheas@autistici.org>
2023-11-18 21:10:15 +0300

    Rename repoId to repoName; Add comments

--- contracts/DegitHub.sol
+++ contracts/DegitHub.sol
@@ -6,42 +6,46 @@ import "@bandada/contracts/protocols/IBandadaSemaphore.sol";
 contract DegitHub {
     IBandadaSemaphore public bandadaSemaphore;
 
-    // Structure for a branch
+    // Structure for representing a branch in a repository
     struct Branch {
         string name;
         string headCommitHash;
     }
 
-    // Structure for a repository
+    // Structure for representing a repository
     struct Repository {
-        uint256 groupId; // Bandada group managing this repository
-        uint256 merkleTreeRoot;
-        uint256 merkleTreeDepth;
-        mapping(string => Branch) branches; // Mapping from branch name to Branch struct
+        uint256 groupId; // Bandada group ID associated with this repository
+        uint256 merkleTreeRoot; // Root of the Merkle tree for Semaphore verification
+        uint256 merkleTreeDepth; // Depth of the Merkle tree
+        mapping(string => Branch) branches; // Mapping of branch names to their corresponding Branch struct
     }
 
-    // Mapping from repository ID to Repository struct
+    // Mapping from repository names to their corresponding Repository struct
     mapping(string => Repository) public repositories;
 
-    // Constructor to initialize the BandadaSemaphore contract
+    // Constructor to set the Bandada Semaphore contract address
     constructor(address _bandadaSemaphoreAddress) {
         bandadaSemaphore = IBandadaSemaphore(_bandadaSemaphoreAddress);
     }
 
-    // Function to add a new repository
-    function addRepository(string memory repoId, uint256 groupId, uint256 merkleTreeRoot, uint256 merkleTreeDepth) public {
-        require(repositories[repoId].groupId == 0, "Repository already exists");
-
-        Repository storage newRepo = repositories[repoId];
+    // Function to add a new repository using its name
+    // repoName: The name of the repository
+    // groupId, merkleTreeRoot, merkleTreeDepth: Semaphore related parameters
+    function addRepository(string memory repoName, uint256 groupId, uint256 merkleTreeRoot, uint256 merkleTreeDepth) public {
+        require(repositories[repoName].groupId == 0, "Repository already exists");
+        Repository storage newRepo = repositories[repoName];
         newRepo.groupId = groupId;
         newRepo.merkleTreeRoot = merkleTreeRoot;
         newRepo.merkleTreeDepth = merkleTreeDepth;
-        // Note: The mapping within the struct is automatically initialized
     }
 
-    // Function to update the branch head
+    // Function to update the head commit hash of a branch in a repository
+    // repoName: The name of the repository
+    // branchName: The name of the branch to update
+    // newHeadCommitHash: The new head commit hash for the branch
+    // signal, nullifierHash, externalNullifier, proof: Semaphore related parameters for verification
     function updateBranchHead(
-        string memory repoId, 
+        string memory repoName, 
         string memory branchName, 
         string memory newHeadCommitHash, 
         uint256 signal, 
@@ -51,10 +55,10 @@ contract DegitHub {
     ) 
         public 
     {
-        Repository storage repo = repositories[repoId];
+        Repository storage repo = repositories[repoName];
         require(repo.groupId != 0, "Repository does not exist");
 
-        // Verify group membership using BandadaSemaphore
+        // Verify the group membership using BandadaSemaphore
         bandadaSemaphore.verifyProof(
             repo.groupId,
             repo.merkleTreeRoot,
@@ -72,9 +76,10 @@ contract DegitHub {
     }
 
     // Function to get the head commit hash of a branch in a repository
-    function getBranchHead(string memory repoId, string memory branchName) public view returns (string memory) {
-        return repositories[repoId].branches[branchName].headCommitHash;
+    // repoName: The name of the repository
+    // branchName: The name of the branch
+    function getBranchHead(string memory repoName, string memory branchName) public view returns (string memory) {
+        return repositories[repoName].branches[branchName].headCommitHash;
     }
 
-    // Additional functions and access control logic can be implemented here
 }
