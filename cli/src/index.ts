836a65749cc0496bf953ff020754a1463063bcb6
Srdjan S <stankovic.srdjo@gmail.com>
2023-11-18 11:41:40 +0100

    Create keys on setup; Add init, commit and push cmds

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,11 +1,16 @@
-const { Command } = require("commander");
-const figlet = require("figlet");
-const path = require("path");
-const os = require("os");
-const fs = require("fs");
+#!/usr/bin/env ts-node --esm
+import { Command } from "commander";
+import figlet from "figlet";
+import path from "path";
+import os from "os";
+import fs from "fs";
+import { exec, execSync } from "child_process";
+import { Config } from "./core/config";
+import { Web3 } from "web3";
+
+const web3 = new Web3(process.env.WEB3_PROVIDER_URL);
 const program = new Command();
 const logo = figlet.textSync("deGit");
-const { execSync } = require("child_process");
 
 program.name("deGit").version("1.0.0").description(logo);
 
@@ -16,11 +21,18 @@ program
     // Setup directory in home directory
     const homeDir = os.homedir();
     const deGitDir = path.join(homeDir, ".degit");
+    const account = web3.eth.accounts.create();
+    const keys = {
+      public: account.address,
+      private: account.privateKey,
+    };
+
     if (!fs.existsSync(deGitDir)) {
       fs.mkdirSync(deGitDir);
+      fs.writeFileSync(path.join(deGitDir, "keys.json"), JSON.stringify(keys));
       console.log(`Created deGit directory at ${deGitDir}`);
     } else {
-      console.log("deGit directory already exists");
+      console.log("degit directory already exists");
     }
   });
 
@@ -37,10 +49,51 @@ program
   .action((relativePath: string, config: Config) => {
     const serializedConfig = JSON.stringify(config);
     const absPath = path.resolve(relativePath);
+    const deGitDir = `${absPath}/.degit`;
+    fs.mkdirSync(deGitDir);
     console.log("init", absPath);
     // Create a new git repository
     execSync(`git init ${absPath}`);
-    fs.writeFileSync(path.join(absPath, ".degit.json"), serializedConfig);
+    fs.writeFileSync(path.join(deGitDir, "config.json"), serializedConfig);
+  });
+
+program
+  .command("add")
+  .argument("<file>")
+  .description("Add file to the staging area")
+  .action((file) => {
+    exec(`git add ${file}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error}`);
+        return;
+      } else if (stderr) {
+        console.error(stderr);
+        return;
+      } else {
+        console.log(stdout);
+      }
+    });
+  });
+
+program
+  .command("commit")
+  .option("-m, --message <string>", "commit message")
+  .description("Commit changes")
+  .action((options) => {
+    exec(
+      `git commit -m "${options.message} --author='Anon <anon@degit.org>'"`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error: ${error}`);
+          return;
+        } else if (stderr) {
+          console.error(stderr);
+          return;
+        } else {
+          console.log(stdout);
+        }
+      }
+    );
   });
 
 program
@@ -48,15 +101,37 @@ program
   .argument("<branch>")
   .description("Push changes to a branch")
   .action((branch: string) => {
-    const output = execSync(`git push ipld:: ${branch}`).toString();
-    const cidRegex = /ipld:\/\/(\w+)/;
-    const match = output.match(cidRegex);
+    exec(`git push ipld:: ${branch}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
 
-    if (match) {
-      const cid = match[1];
-      console.log(`CID: ${cid}`);
-    } else {
-      console.log("No CID found in the output");
-    }
+      const output = stderr.trim();
+      const cidRegex = /ipld:\/\/(\w+)/;
+      const match = output.match(cidRegex);
+
+      exec(
+        "git rev-parse --abbrev-ref HEAD",
+        (branchError, branchStdout, branchStderr) => {
+          if (branchError) {
+            console.error(`exec error: ${branchError}`);
+            return;
+          }
+
+          const currentBranch = branchStdout.trim();
+
+          if (match) {
+            const cid = match[1];
+            console.log(`CID: ${cid}`);
+          } else {
+            console.log("No CID found in the output");
+          }
+
+          console.log("Current branch: ", currentBranch);
+        }
+      );
+    });
   });
+
 program.parse(process.argv);
